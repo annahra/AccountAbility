e30adfc04cc0ecc741f896f9ca72b303aab8e307
auna <annah.ramones@gmail.com>
2020-06-30 22:45:20 -0700

    [annah] added category segment and displayed news objects

--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Router, NavigationExtras } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
 
-  constructor() { }
+  selected = 'general';
+  news = [];
+
+  constructor(private data: DataService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
+    this.loadData();
+  }
+
+  async loadData() {
+    let loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    this.data.getTopNews(this.selected).subscribe(res => {
+      loading.dismiss();
+      this.news = res;
+    });
+  }
+
+  segmentChanged(e) {
+    this.loadData();
+  }
+
+  open(n) {
+    console.log(n);
+    let navigationExtras: NavigationExtras = {
+
+    }
   }
 
 }
